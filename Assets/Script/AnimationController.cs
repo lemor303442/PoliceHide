b370361e52a0d337e1d4316970e863b1b0b658a5
KanekoYuto <kanekoyuuto@gmail.com>
2017-04-25 14:46:29 +0900

    PlayerAnimation追加&&EnemySiteの追加

---
+++ Assets/Script/AnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+	
+	public Animator PlayerAnimator;
+	AnimatorStateInfo animInfo;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animInfo = PlayerAnimator.GetCurrentAnimatorStateInfo(0);
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.gameObject.tag == "Eyesite") {
+			if (animInfo.nameHash != Animator.StringToHash ("Base Layer.Grounded")) {
+				Debug.Log ("GameOver");
+			}
+		}
+	}
+	public void DanceAnimation(){
+		PlayerAnimator.SetTrigger ("Dance");
+	}
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
-	public GameObject player;
+public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +11,7 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.gameObject.transform.position = new Vector3 (player.transform.position.x, 1.26f, -7.55f);
+		
 	}
+		
 }
