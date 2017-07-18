4875e770518b1b8ee28d197308a10bb9582f9aa8
lemor303442 <yuki.303442@gmail.com>
2017-07-18 15:56:58 +0900

    startActionAnimationを修正

--- Assets/Scripts/Police/BaseBehaviors/StartActionAnimation.cs
+++ Assets/Scripts/Police/BaseBehaviors/StartActionAnimation.cs
@@ -6,18 +6,24 @@ namespace Police
 {
 	public class StartActionAnimation : PoliceAction {
 		private Animator m_animator;
+		private string m_anim;
 		private bool m_waitAnimEnd;
+		private bool m_startAnim;
 
 		public void Init(string anim, bool waitAnimEnd)
 		{
 			m_animator = m_gameObject.GetComponent<Animator>();
-			m_animator.Play(anim, 0);
+			m_anim = anim;
 			m_waitAnimEnd = waitAnimEnd;
+			m_startAnim = true;
 		}
 
 		// Update is called once per frame
 		public override void Update(){
-		
+			if(m_startAnim){
+				m_animator.Play(m_anim, 0);
+				m_startAnim = false;
+			}
 		}
 
 		public override bool IsEnd(){
