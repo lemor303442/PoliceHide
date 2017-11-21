0c5a58ce6a1bc4c441d8471d8a159141a95a69ae
lemor303442 <yuki.303442@gmail.com>
2017-11-21 14:37:00 +0900

    csvManagerの更新

--- Assets/Plugins/CSVManager/csvManager.cs
+++ Assets/Plugins/CSVManager/csvManager.cs
@@ -2,134 +2,49 @@
 using System.Collections;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
 
 
 public class csvManager
 {
-	//CSVデータを読み込んで、行と列を配列で返す関数
-	public int[] getCSVLength (string dataPath)
+	/// <summary>
+	/// Resourcesフォルダ内のcsvファイルを読み込み、string型2次元配列で返す。
+	/// </summary>
+	public static string[,] ReadCsvFile (string dataPath)
 	{
 		string data;
-		//データが存在するか確認/なければnullを返す
-		if (File.Exists (dataPath) == false) {
-			return null;
+		string textData = OpenTextFile (GetPath () + dataPath);
+		if (textData != "ERROR") {
+			data = textData;
 		} else {
-			//ファイルを読み込む
-			FileStream fs = new FileStream (dataPath, FileMode.Open);
-			StreamReader sr = new StreamReader (fs);
-			data = sr.ReadToEnd ();
-			sr.Close ();
-		}
-		string[] rows = data.Replace ("\r\n", "\n").Split ('\n');
-
-		//行数dataRowsと列数dataColsを取得する
-		int dataRows;
-		int dataCols;
-		dataRows = rows.Length;
-		string[] cols = rows [0].Split ("," [0]);
-		dataCols = cols.Length;
-
-		//int型配列にdataRowsとdataColsを代入する
-		int[] getLength = new int[2];
-		getLength [0] = dataRows;
-		getLength [1] = dataCols;
-		return getLength;
-	}
-
-
-	//CSVデータを読み込んで、string型2次元配列で返す関数
-	public string[,] ReadFile (string dataPath)
-	{
-		string data;
-		//データが存在するか確認/なければnullを返す
-		if (File.Exists (dataPath) == false) {
-			return null;
-		} else {
-			//ファイルを読み込む
-			FileStream fs = new FileStream (dataPath, FileMode.Open);
-			StreamReader sr = new StreamReader (fs);
-			data = sr.ReadToEnd ();
-			sr.Close ();
-		}
-		//読み込んだファイルを分断する
-		string[] rows = data.Replace ("\r\n", "\n").Split ('\n');
-		//行数dataRowsと列数dataColsを取得する
-		int dataCols;
-		int dataRows;
-		dataRows = rows.Length;
-		string[] cols = rows [0].Split ("," [0]);
-		dataCols = cols.Length;
-		//List型宣言
-		List<string[]> csvList = new List<string[]> ();
-		//rowsをsplitして、string配列colsに代入→それぞれのcolsをList型csvDataにAddする
-		for (int i = 0; i < dataRows; i++) {
-			cols = rows [i].Split ("," [0]);
-			csvList.Add (cols);
+			TextAsset textAsset = (TextAsset)Resources.Load (dataPath.Split ("." [0]) [0]);
+			data = textAsset.ToString ();
 		}
 
-		//2次元配列の宣言
-		string[,] csvData = new string[dataRows, dataCols];
-		//List型→string[]型に変換
-		int m = 0;
-		foreach (string[] str in csvList) {
-			for (int i = 0; i < str.Length; i++) {
-				csvData [m, i] = str [i];
-			}
-			m++;
-		}
-
-		//戻り値
-		return csvData;
-	}
-
-	//ResourceフォルダからCSVデータを読み込んで、string型2次元配列で返す関数
-	public static string[,] ReadFilesInResources (string dataPath)
-	{
-		//改行ごとに要素分けいた配列rowsを作成する
-		TextAsset data = (TextAsset)Resources.Load (dataPath);
-		//もし参照してなければ、nullを返す
 		if (data == null) {
 			return null;
 		}
-		string[] rows = data.text.Replace ("\r\n", "\n").Split ('\n');
-
-		//行数dataRowsと列数dataColsを取得する
-		int dataCols;
-		int dataRows;
-		dataRows = rows.Length;
-		string[] cols = rows [0].Split ("," [0]);
-		dataCols = cols.Length;
-		//List型宣言
-		List<string[]> csvList = new List<string[]> ();
-		//rowsをsplitして、string配列colsに代入→それぞれのcolsをList型csvDataにAddする
-		for (int i = 0; i < dataRows; i++) {
-			cols = rows [i].Split ("," [0]);
-			csvList.Add (cols);
-		}
-
-		//2次元配列の宣言
+		string[] rows = data.Replace ("\r\n", "\n").Split ('\n');
+		int dataRows = rows.Length;
+		int dataCols = rows [0].Split ("," [0]).Length;
 		string[,] csvData = new string[dataRows, dataCols];
-		//List型→string[]型に変換
-		int m = 0;
-		foreach (string[] str in csvList) {
-			for (int i = 0; i < str.Length; i++) {
-				csvData [m, i] = str [i];
-				//Debug.Log(str[i]);
+		for (int i = 0; i < csvData.GetLength (0); i++) {
+			for (int j = 0; j < csvData.GetLength (1); j++) {
+				string[] value = rows [i].Split ("," [0]);
+				csvData [i, j] = value [j];
 			}
-			m++;
 		}
-
-		//戻り値
 		return csvData;
 	}
-		
 
-	//CSVデータに2次元配列を上書きする
-	public void WriteData (string dataPath, string[,] newData)
+
+	/// <summary>
+	/// Resourcesフォルダ内に、ファイルを書き込む。
+	/// 例) csvManager.WriteData("data.csv", dataArray);
+	/// </summary>
+	public static void WriteData (string dataPath, string[,] newData)
 	{
-		//2次元配列をまとめるstring型変数の宣言
 		string stringData = "";
-		//2次元配列をまとめるstring型変数にまとめる
 		for (int i = 0; i < newData.GetLength (0); i++) {
 			for (int j = 0; j < newData.GetLength (1); j++) {
 				if (j < newData.GetLength (1) - 1) {
@@ -141,48 +56,39 @@ public class csvManager
 				}
 			}
 		}
-		//データpathを設定
-		//string datapath = DataPath;
-		//データを書き込む
-		FileStream fs = new FileStream (dataPath, FileMode.Create);
+		Debug.Log (GetPath () + dataPath);
+		FileStream fs = new FileStream (GetPath () + dataPath, FileMode.Create);
 		StreamWriter sw = new StreamWriter (fs);
 		sw.Write (stringData);
 		sw.Flush ();
 		sw.Close ();
 	}
 
-	//string2次元配列に新しい行を追加する
-	public string[,] AddStringArrayToCSV (string[,] arrayData, string[] data)
-	{
-		//配列の長さがあっているか確認/なければnullを返す
-		if (arrayData.GetLength (1) != data.Length) {
-			return null;
-		} else {
-			string[,] newArrayData = new string[arrayData.GetLength (0) + 1, arrayData.GetLength (1)];
-			for (int i = 0; i < arrayData.GetLength (0) + 1; i++) {
-				for (int j = 0; j < arrayData.GetLength (1); j++) {
-					if (i == arrayData.GetLength (0)) {
-						newArrayData [i, j] = data [j];
-					} else {
-						newArrayData [i, j] = arrayData [i, j];
-					}
-				}
-			}
-			return newArrayData;
-		}
-	}
 
-	public string getPath ()
+	public static string GetPath ()
 	{
 		#if UNITY_EDITOR
-		return Application.dataPath + "/Resources";
+		return Application.dataPath + "/Resources/";
 		#elif UNITY_ANDROID
-		return Application.persistentDataPath + "";
+		return Application.persistentDataPath + "/";
 		#elif UNITY_IPHONE
-		return Application.persistentDataPath + "";
+		return Application.persistentDataPath + "/";
 		#else
 		return Application.dataPath + "";
 		#endif
 	}
+
+	public static string OpenTextFile (string _filePath)
+	{
+		FileInfo fi = new FileInfo (_filePath);
+		string returnSt = "";
+		if (fi.Exists) {
+			StreamReader sr = new StreamReader (fi.OpenRead (), Encoding.UTF8);
+			returnSt = sr.ReadToEnd ();
+			sr.Close ();
+		} else {
+			returnSt = "ERROR";
+		}
+		return returnSt;
+	}
 }
-			
